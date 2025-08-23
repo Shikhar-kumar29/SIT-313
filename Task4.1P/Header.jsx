36ced210d3e84eaf381c23f15fff22a4d5d9d9c5
Shikhar_29 <shikhar4883.se24@chitkara.edu.in>
2025-08-23 13:03:07 +0530

    Create Header.jsx

---
+++ Task4.1P/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Welcome to DEV@Deakin</h1>
+      <p>Your place to learn, share, and grow as a developer.</p>
+    </header>
+  );
+}
+
+export default Header;
