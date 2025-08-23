f31d3e95b077e411d52f3a12aec4b4120dd14ad1
Shikhar_29 <shikhar4883.se24@chitkara.edu.in>
2025-08-23 13:03:34 +0530

    Create Hero.jsx

---
+++ Task4.1P/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <img 
+        src="https://picsum.photos/800/300" 
+        alt="image Banner" 
+        style={{ width: "100%", borderRadius: "8px" }}
+      />
+    </div>
+  );
+}
+
+export default Hero;
