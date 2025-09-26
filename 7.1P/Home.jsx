c98e575a26a1d786f613bea948e3fee5ac1880f1
Shikhar_29 <shikhar4883.se24@chitkara.edu.in>
2025-09-26 15:03:04 +0530

    Create Home.jsx

---
+++ 7.1P/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h2>Welcome to DEV@Deakin!</h2>
+      <p>You have successfully logged in.</p>
+    </div>
+  );
+}
