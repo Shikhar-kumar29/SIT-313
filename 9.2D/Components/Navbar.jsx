d8dc91bdd930380835263f7332a0d6b395f13d0e
Shikhar_29 <shikhar4883.se24@chitkara.edu.in>
2025-11-11 00:12:31 +0530

    Update Navbar.jsx

--- 9.2D/Components/Navbar.jsx
+++ 9.2D/Components/Navbar.jsx
@@ -1,30 +1,96 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Menu, Container, Icon } from 'semantic-ui-react';
+import { useAuth } from '../context/AuthContext';
 
 function Navbar() {
+  const { user, logout, loading, isPremium } = useAuth();
+  const navigate = useNavigate();
+  const navItemStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '0.5rem',
+    color: 'inherit',
+    textDecoration: 'none'
+  };
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate('/');
+    } catch (err) {
+      console.error('Failed to sign out:', err);
+    }
+  };
+
   return (
-    <Menu inverted color='blue' size='large' style={{ margin: 0, borderRadius: 0 }}>
+    <Menu inverted color='blue' size='large' style={{ margin: 0, borderRadius: 0, borderBottom: 'none', boxShadow: 'none' }}>
       <Container>
         <Menu.Item as={Link} to='/' header>
           <Icon name='code' />
           DEV@Deakin
         </Menu.Item>
-        <Menu.Item as={Link} to='/'>
+        <Menu.Item as={Link} to='/' style={navItemStyle}>
           <Icon name='home' />
-          Home
+          <span>Home</span>
         </Menu.Item>
-        <Menu.Item as={Link} to='/post'>
-          <Icon name='pencil' />
-          Post
-        </Menu.Item>
-        <Menu.Item as={Link} to='/find-questions'>
-          <Icon name='search' />
-          Find Questions
-        </Menu.Item>
-        <Menu.Item as={Link} to='/plans'>
-          <Icon name='star' />
-          Plans
-        </Menu.Item>
+        {user && (
+          <>
+            <Menu.Item as={Link} to='/post' style={navItemStyle}>
+              <Icon name='pencil' />
+              <span>Post</span>
+            </Menu.Item>
+            <Menu.Item as={Link} to='/find-questions' style={navItemStyle}>
+              <Icon name='search' />
+              <span>Find Questions</span>
+            </Menu.Item>
+          </>
+        )}
+        {!loading && isPremium && (
+          <>
+            <Menu.Item as={Link} to='/premium/analytics' style={navItemStyle}>
+              <Icon name='chart line' />
+              <span>Analytics Dashboard</span>
+            </Menu.Item>
+            <Menu.Item as={Link} to='/premium/support' style={navItemStyle}>
+              <Icon name='life ring' />
+              <span>Priority Support</span>
+            </Menu.Item>
+            <Menu.Item as={Link} to='/post' style={navItemStyle}>
+              <Icon name='code' />
+              <span>Advanced Code Editor</span>
+            </Menu.Item>
+          </>
+        )}
+        {!loading && !isPremium && (
+          <Menu.Item as={Link} to='/plans' style={navItemStyle}>
+            <Icon name='tasks' />
+            <span>Plans</span>
+          </Menu.Item>
+        )}
+        <Menu.Menu position='right'>
+          {loading && (
+            <Menu.Item disabled style={navItemStyle}>
+              <Icon name='spinner' loading />
+              <span>Loading</span>
+            </Menu.Item>
+          )}
+          {!loading && (
+            <>
+              {!user && (
+                <Menu.Item as={Link} to='/login' style={navItemStyle}>
+                  <Icon name='sign in' />
+                  <span>Sign In</span>
+                </Menu.Item>
+              )}
+              {user && (
+                <Menu.Item onClick={handleLogout} style={navItemStyle}>
+                  <Icon name='sign out' />
+                  <span>Sign Out</span>
+                </Menu.Item>
+              )}
+            </>
+          )}
+        </Menu.Menu>
       </Container>
     </Menu>
   );
